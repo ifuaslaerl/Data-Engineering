ebf05511b4b8ef007a031cdd8ea1ee81db009bdc
Luís Rafael Sena <luisrafaelsen.20231@poli.ufrj.br>
2024-08-21 16:41:49 -0300

    Update and rename Training.py to main.py

--- Sonar Passivo/main.py
+++ Sonar Passivo/main.py
@@ -1,39 +1,35 @@
 #Bibliotecas
-
 import torch
 import torchvision.transforms as transforms
-import MatDataset
-import Networks
+#include Networks
+#include MatDataset
 
 BS = 1 # tamanho dos conjuntos trabalhados
+data = '/content/DadosSonar'
 
-# Dataset
+def main():
 
-data = 'Sonar Passivo/DadosSonar'
+  trainset = MatDataset(os.path.join(data,'train'))
 
-trainset = MatDataset.MatDataset(f'{data}/train')
+  testset = MatDataset(os.path.join(data,'test'))
 
-testset = MatDataset.MatDataset(f'{data}/test')
+  validateset = MatDataset(os.path.join(data,'validate'))
 
-validateset = MatDataset.MatDataset(f'{data}/validate')
-
-trainloader = torch.utils.data.DataLoader(trainset,
+  trainloader = torch.utils.data.DataLoader(trainset,
                                         batch_size=BS,
                                         shuffle=True,
                                         num_workers=2)
 
-testloader = torch.utils.data.DataLoader(testset,
+  testloader = torch.utils.data.DataLoader(testset,
                                         batch_size=BS,
                                         shuffle=True,
                                         num_workers=2)
 
-validateloader = torch.utils.data.DataLoader(validateset,
+  validateloader = torch.utils.data.DataLoader(validateset,
                                         batch_size=BS,
                                         shuffle=True,
                                         num_workers=2)
 
-model = Networks.Sonar_CNN()
+  model = Sonar_CNN()
 
-Networks.fit(model,trainloader,validateloader,root="Sonar Passivo")
-
-
+  fit(model,trainloader,validateloader,root="Sonar Passivo")
